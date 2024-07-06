9c44c990df4cc0e67cf7dfa0f22f5a9c4523e993
Robert Wan <wantunglok@gmail.com>
2024-07-06 12:08:43 +1000

    add stub code for login

--- app/model/User.py
+++ app/model/User.py
@@ -2,3 +2,4 @@ class User:
     def __init__(self):
         self.username = "wanlok"
         self.hashed_password = "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW"
+        self.status = "Enabled"
--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 from app.controller import DummyController
 from app.controller import UserController
 
 app = FastAPI()
+
+origins = ["*"]
+
+# * add middleware
+app.add_middleware(
+    middleware_class=CORSMiddleware,
+    allow_origins=origins,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 app.include_router(DummyController.router)
 app.include_router(UserController.router)
